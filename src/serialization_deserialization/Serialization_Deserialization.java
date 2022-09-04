77628aa30dcfafe8c6997ce4f5b76ea6f45dd98b
sokoeurn88 <92101403+sokoeurn88@users.noreply.github.com>
2022-09-03 19:14:51 -0500

    serialization and deserialization

---
+++ src/serialization_deserialization/Serialization_Deserialization.java
@@ -0,0 +1,10 @@
+package serialization_deserialization;
+
+public class Serialization_Deserialization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/serialization_deserialization/Student.java
@@ -0,0 +1,16 @@
+package serialization_deserialization;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+	
+	int id;
+	String name;
+	
+	public Student(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+
+}
